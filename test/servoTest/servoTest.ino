447cd876ea6920d340a36bff7807dd7e0396df55
Blaze_Utz <e139009@rrisd.local>
2021-03-03 17:26:28 -0600

    Improved servo test file for multiple positions

--- test/servoTest/servoTest.ino
+++ test/servoTest/servoTest.ino
@@ -1,17 +1,25 @@
 #include <Wire.h>
 #include <Servo.h>
-#include <Encoder.h>
-#include <HCSR04.h>
 Servo serv;
   
 void setup() {
   serv.attach(4); //TODO: attach servo pin
+  serv.write(90); // set servo to angle pos
 
- 
 }
 
 void loop() {
- 
-
-  serv.write(130); // set servo to angle pos
+ for (int i = 90; i > 0; i--){
+  serv.write(i);
+  delay(5);
+ }
+ for (int i = 0; i < 180; i++){
+  serv.write(i);
+  delay(5);
+ }
+ for (int i = 180; i > 90; i--){
+  serv.write(i);
+  delay(5);
+ }
+ delay(5000);
 }
